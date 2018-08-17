2e43ff75b4027863c2e833212724a4d8a2ad59e9
llchen <llchen@telenav.cn>
2018-08-17 19:07:00 +0800

    Use @Parameters in JUnit

--- LogAn/src/LogAnalyzerTest.java
+++ LogAn/src/LogAnalyzerTest.java
@@ -1,9 +1,18 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.Collection;
 
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
 class LogAnalyzerTest {
 
+	/*
 	@Test
 	void isValidFileName_BadExtention_ReturnFalse() {
 		LogAnalyzer analyzer = new LogAnalyzer();
@@ -24,5 +33,28 @@ class LogAnalyzerTest {
 		boolean result = analyzer.isValidFileName("fileWithBadExtention.SLF");
 		assertTrue(result);
 	}
+	*/
 
+	@Parameters
+	public static Object[][] data() {
+		return new Object[][] {
+			{"fileWithBadExtention.foo", false},
+			{"fileWithGoodExtention.slf", true},
+			{"fileWithGoodExtention.SLF", true}
+		};
+	}
+	
+	@Parameter(0)
+	public String fileName;
+	
+	@Parameter(1)
+	public boolean expectResult;
+	
+	@Test
+	public void isValidFileName() {
+		LogAnalyzer analyzer = new LogAnalyzer();
+		boolean actualResult = analyzer.isValidFileName(fileName);
+		assertEquals(expectResult, actualResult);
+	}
+	
 }
