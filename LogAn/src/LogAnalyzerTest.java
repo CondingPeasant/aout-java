8fd7cc6dd65fcab20e6c5ac357ec4427de1c30c2
llchen <llchen@telenav.cn>
2018-08-24 18:13:05 +0800

    With error: org.junit.jupiter.api.extension.ParameterResolutionException: No ParameterResolver registered for parameter

--- LogAn/src/LogAnalyzerTest.java
+++ LogAn/src/LogAnalyzerTest.java
@@ -6,26 +6,28 @@ import java.util.Collection;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameter;
 import org.junit.runners.Parameterized.Parameters;
 
 @RunWith(Parameterized.class)
 class LogAnalyzerTest {
 
 	@Parameters
-	public static Object[][] data() {
-		return new Object[][] {
+	public static Collection<Object[]> data() {
+		return Arrays.asList(new Object[][] {
 			{"fileWithBadExtention.foo", false},
 			{"fileWithGoodExtention.slf", true},
 			{"fileWithGoodExtention.SLF", true}
-		};
+		});
 	}
 	
-	@Parameter(0)
-	public String fileName;
+	private String fileName;
 	
-	@Parameter(1)
-	public boolean expectResult;
+	private boolean expectResult;
+	
+	public LogAnalyzerTest(String fileName, boolean expectResult) {
+		this.fileName = fileName;
+		this.expectResult = expectResult;
+	}
 	
 	@Test
 	public void isValidFileName() {
